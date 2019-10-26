55318ac5d7da3badce287baa10bc213b8c631f1b
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-10-26 10:12:04 -0700

    add rsvps

---
+++ app/controllers/rsvps_controller.rb
@@ -0,0 +1,13 @@
+class RsvpsController < ApplicationController
+
+  # POST: /rsvps
+  post "/rsvps" do
+    redirect "/rsvps"
+  end
+
+  # PATCH: /rsvps/5
+  patch "/rsvps/:id" do
+    redirect "/rsvps/:id"
+  end
+
+end
